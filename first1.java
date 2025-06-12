83ccc8f34e881874a8fee0001cd0f1d00a710e5a
sumedhpatil2005 <sumedhpatil2005@gmail.com>
2025-06-12 21:20:44 +0530

    basic

---
+++ first1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class first1 {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        System.out.println(n);
+ 
+        
+    }
+}
